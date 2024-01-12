6d7477b9f634949c162399e446108872d9f04efc
Enzo Rossetto <enzo.rossetto@ifood.com.br>
2024-01-11 22:12:26 -0300

    feat: implement game logic with Story, StoryBrain and ViewController logic using MVC patthern

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUi()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        if let userChoice = sender.currentTitle {
+            storyBrain.nextStory(userChoice: userChoice)
+            updateUi()
+        }
+    }
+    
+    func updateUi() {
+        let story = storyBrain.getCurrentStory()
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
+    }
+    
 }
 
